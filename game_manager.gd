974a72c2e95e03732ab89cb52eccf253918eb0ab
emanswed <emanswed482@gmail.com>
2025-08-28 10:39:09 +0300

    eman's update3

--- game_manager.gd
+++ game_manager.gd
@@ -5,4 +5,4 @@ var score = 0
 
 func add_point():
 	score += 1
-	score_label.text= "You collected "+ str(score)+ " coins."
+	score_label.text= "Coin Score: "+ str(score)
--- scripts/coins.gd
+++ scripts/coins.gd
@@ -8,14 +8,9 @@ extends Area2D
 #func _on_body_entered(body):
 	#print("+1 coin")
 
-#@onready var game_manager: Node = %GameManager
-#@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 #
-#func _on_body_entered(body):
-	#game_manager.add_point()
-	#animation_player.play("pickup_animation")
-	#
-@onready var game_manager = %GameManager
 func _on_body_entered(body):
 	game_manager.add_point()
-	queue_free()
+	animation_player.play("pickup_animation")
